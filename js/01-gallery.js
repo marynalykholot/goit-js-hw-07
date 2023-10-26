7226effa125ddaf37b2988ce12ee13ae68ac428b
Maryna Lykholot <marina.litkovets96@gmail.com>
2023-10-26 21:02:40 +0300

    prittier

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const list = document.querySelector(".gallery");
 
-const list = document.querySelector('.gallery');
-
-list.insertAdjacentHTML("beforeend",createGalleryMarkup(galleryItems));
-list.addEventListener("click",handleClick);
-
+list.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+list.addEventListener("click", handleClick);
 
 function createGalleryMarkup(galleryItems) {
-    return galleryItems
-      .map(({ description, original, preview }) => {
-        return `
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
           <div class="gallery__item">
           <a class="gallery__link" href="${original}">
             <img
@@ -23,38 +21,33 @@ function createGalleryMarkup(galleryItems) {
           </a>
         </div>
         `;
-      })
-      .join("");
+    })
+    .join("");
+}
+
+function handleClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
   }
 
-function handleClick(evt){
-    evt.preventDefault();
-    
-    if (evt.target.nodeName !== 'IMG') {
-        return;
-    }
-
-
-        const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
         <img
         class="gallery__image"
         src="${evt.target.dataset.source}"
         width="800" height="600"
       />
-      `)
-      // console.log(instance);
-      instance.show();
+      `);
+  // console.log(instance);
+  instance.show();
+}
 
-     
-
-    }
- 
-   
-    // if (instance.visible()) {
-    //     list.addEventListener('keydown', (e) => {
-    //         if (e.code === 'Escape') {
-    //             instance.close()
-    //         }
-    //     }
-    //     );
-    // }
+// if (instance.visible()) {
+//     list.addEventListener('keydown', (e) => {
+//         if (e.code === 'Escape') {
+//             instance.close()
+//         }
+//     }
+//     );
+// }
